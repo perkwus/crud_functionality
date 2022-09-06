e2d046be384b7d81ecbd699bf04a3f80e28f5ef5
Trace Fleeman y Garcia <tracefleeman@gmail.com>
2022-09-06 13:50:44 -0700

    Adds CRUD functionality

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const db = require("./db")
 
 const app = express()
+
+app.use(express.json())
 
 app.get("/", (req, res) => {
     console.log(req)
@@ -14,14 +17,39 @@ app.get("/users/:user", (req, res) => {
     })
 })
 
-app.get("/hello", helloHandler)
+app.get("/users", getAllUsers)
+function getAllUsers(req, res) {
+    let allUsers = db.getUsers();
 
-function helloHandler(req, res) {
-    console.log(req)
-    res.send("<h1>hewwo</h1>")
+    return res.status(200).json(allUsers)
+}
+
+app.post("/users", createUser)
+function createUser(req, res) {
+    let id = db.addUser({ name: req.body.user })
+
+    return res.status(201).json({ created: true, id})
+}
+
+app.patch("/users/:id", updateUser)
+function updateUser(req, res) {
+    let name = req.body.user
+    let {id} = req.params
+
+    db.updateUser({ id, name })
+
+    return res.status(200).json({ id, name })
+}
+
+app.delete("/users/:id", deleteUser)
+function deleteUser(req, res) {
+    let {id} = req.params
+
+    db.deleteUser(id)
+
+    return res.status(200).json({ deleted: true, id })
 }
 
 app.listen(3000, () => {
-    console.log("penis2")
-}
-)
+    console.log("listening on port 3000 (penis)")
+})
